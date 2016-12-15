673635f38eee27dca5ea38e529513074ed42da5b
zab88 <alexey.zabaykin@gmail.com>
2016-12-15 20:45:46 +0700

    setup script improved

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from distutils.core import setup
 import py2exe
 
-setup(console=['main.py'])
+setup(
+    options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
+    console=['main.py'],
+    zipfile = None
+)
 # run "setup.py py2exe" to make exe file
